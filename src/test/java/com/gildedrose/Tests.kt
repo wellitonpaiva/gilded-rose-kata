f37fda2f343348c5dcf8da4e8503bc7bb6c4b355
Welliton Paiva <wellitonpaiva@gmail.com>
2023-05-31 21:52:00 +0100

    adding items to stock

--- src/test/java/com/gildedrose/Tests.kt
+++ src/test/java/com/gildedrose/Tests.kt
@@ -9,6 +9,8 @@ class Tests {
     @Test
     fun test() {
         val stock = listOf<Item>()
+        assertEquals(listOf<Item>(), stock)
+
         val newStock = stock + Item("banana", LocalDate.now(), 42u)
 
         assertEquals(listOf(Item("banana", LocalDate.now(), 42u)), newStock)
