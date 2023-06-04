41498aa896929a90bb80f852d9273b610e0d6bf3
Welliton Paiva <wellitonpaiva@gmail.com>
2023-06-04 18:14:19 +0100

    adding new way of reading a file

--- src/main/java/com/gildedrose/persisting.kt
+++ src/main/java/com/gildedrose/persisting.kt
@@ -1,20 +1,49 @@
 package com.gildedrose
 
 import java.io.File
+import java.io.IOException
+import java.time.DateTimeException
+import java.time.Instant
 import java.time.LocalDate
 
-fun List<Item>.saveTo(file: File) {
+fun StockList.saveTo(file: File) =
     file.writer().buffered().use { writer ->
-        forEach { item -> writer.appendLine(item.toLine()) }
+        toLines().forEach { line -> writer.appendLine(line) }
     }
-}
 
+fun StockList.toLines(): Sequence<String> =
+    sequenceOf("# LastModified: ${this.lastModified}") +
+        items.map { it.toLine() }
+
+
+fun File.loadItems(defaultLastModified: Instant = Instant.now()): StockList = useLines { lines ->
+    lines.toStockList(defaultLastModified)
+}
 private fun Item.toLine() = "$name\t$sellByDate\t$quality"
 
-fun File.loadItems(): List<Item> =
-    useLines { lines ->
-        lines.map { line -> line.toItem() }.toList()
-    }
+fun Sequence<String>.toStockList(
+    defaultLastModified: Instant
+): StockList {
+    val (header, body) = partition { it.startsWith("#") }
+    return StockList(
+        lastModified = lastModifiedFrom(header) ?: defaultLastModified,
+        items = body.map { line -> line.toItem() }.toList()
+    )
+}
+
+private fun lastModifiedFrom(
+    header: List<String>
+) = (header
+    .lastOrNull{it.startsWith("# LastModified:")}
+    ?.substring("# LastModified:".length)
+    ?.trim()
+    ?.toInstant())
+
+private fun String.toInstant() = try {
+    Instant.parse(this)
+} catch (x: DateTimeException) {
+    throw IOException("Could not parse LastModified header: " + x.message)
+}
 
 private fun String.toItem(): Item {
     val parts = this.split('\t')
@@ -24,3 +53,4 @@ private fun String.toItem(): Item {
         quality = parts[2].toUInt()
     )
 }
+
